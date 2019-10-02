69f6657c3026aa203dfb10a95c2b9fcbea48420e
Bradley Allen <allen.bradley@ymail.com>
2019-10-02 10:54:41 -0500

    sample.sh   begin work

--- sample.sh
+++ sample.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# 	sample.sh  0.1.1.3  2019-10-02T10:54:41.605738-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 'No-parent-git-tag-found-in-repository'  
+# 	   sample.sh   begin work 
 ###  sample.sh
 echo    "Hello world"
+echo    "create a new branch"
+echo    "what branch: remotes/origin/git-TEST-commit-#1"
