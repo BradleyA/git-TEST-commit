858fd5671042432ee95aa2cec4e297cc6093e3ce
Bradley Allen <allen.bradley@ymail.com>
2019-10-02 11:16:24 -0500

    program1.sh   #1   updated file

--- program1.sh
+++ program1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# 	program1.sh  0.1.1.git-TEST-commit-#1.2.4  2019-10-02T11:16:24.125954-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 0.1.1.git-TEST-commit-#1.1  
+# 	   program1.sh   #1   updated file 
 # 	program1.sh  0.1.1.git-TEST-commit-#1.1.3  2019-10-02T11:08:38.809135-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 'No-parent-git-tag-found-in-repository'  
 # 	   first markit in branch remotes/origin/git-TEST-commit-#1 
 ###  program1.sh
 echo    "Hello world"
+echo   "update file1"
