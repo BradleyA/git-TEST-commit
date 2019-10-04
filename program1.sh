1392d24343b6cb67920406da82aea808448fa63d
Bradley Allen <allen.bradley@ymail.com>
2019-10-04 10:27:54 -0500

    program1.sh

--- program1.sh
+++ program1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	program1.sh  0.1.2.dev.1.14  2019-10-04T10:27:54.693874-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 0.1.2-2-ge640bee  
+# 	   program1.sh 
 # 	program1.sh  0.1.2.11  2019-10-02T11:32:15.601852-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 0.1.1.git-TEST-commit-#1.5-2-gf105a16  
 # 	   merged files 
 # 	program1.sh  0.1.1.git-TEST-commit-#1.3.5  2019-10-02T11:18:07.465412-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 0.1.1.git-TEST-commit-#1.2  
@@ -12,3 +14,4 @@ echo    "Hello world"
 echo   "update file1"
 echo   "update file2"
 echo   "branches are merged into master"
+echo   "branch development'
