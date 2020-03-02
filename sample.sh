b2c1ab9ef2cb7cca95aba2af0a5540fd4eb3352f
Bradley Allen <allen.bradley@ymail.com>
2020-03-02 15:19:28 -0600

    sample.sh   testing REPOSITORY_DIR_COUNT

--- sample.sh
+++ sample.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# 	sample.sh  0.1.3.dev.6.13  2020-02-26T22:31:23.680198-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.2-1-g9e5a77e  
+# 	sample.sh  0.1.3.dev.7.18  2020-03-02T15:19:28.184842-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.6-4-g2df435c  
+# 	   sample.sh   testing REPOSITORY_DIR_COUNT 
+# 	sample.sh  0.1.3.dev.6.13  2020-02-26T22:31:23.680198-06:00 (CST)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.2-1-g9e5a77e 
 # 	   sample.sh   testing 
 # 	sample.sh  0.1.1.3  2019-10-02T10:54:41.605738-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  uadmin  five-rpi3b.cptx86.com 'No-parent-git-tag-found-in-repository' 
 # 	   sample.sh   begin work 
