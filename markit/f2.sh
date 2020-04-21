e99adeaed6afb4064538cd0b4933e8d585fe6d12
Bradley Allen <allen.bradley@ymail.com>
2020-04-20 21:39:29 -0500

    markit/f2.sh markit/f3.sh -->   begin testing - automate adding file name(s) to beginning of message

--- markit/f2.sh
+++ markit/f2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	markit/f2.sh  0.1.3.dev.19.38  2020-04-20T21:39:29.606135-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.18  
+# 	   markit/f2.sh markit/f3.sh -->   begin testing - automate adding file name(s) to beginning of message  
 # 	markit/f2.sh  0.1.3.dev.17.36  2020-04-20T20:57:24.728331-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.16  
 # 	   markit/f1.sh markit/f2.sh markit/f3.sh -->   5 test using markit and adding ${FILE_LIST} -->  
 # 	markit/f2.sh  0.1.3.dev.16.35  2020-04-20T20:49:45.950958-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.15  
@@ -6,3 +8,4 @@
 # 	markit/f2.sh  0.1.3.dev.12.31  2020-04-20T19:19:50.815896-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.11  
 # 	   This is a test using markit and adding ${FILE_LIST} --> 
 ###
+
