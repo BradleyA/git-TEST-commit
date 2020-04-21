1d32cc67636f51b221833c4cc24a8c4ec70b9684
Bradley Allen <allen.bradley@ymail.com>
2020-04-20 21:42:49 -0500

    markit/f4.sh -->   begin testing - automate adding file name(s) to beginning of message

---
+++ markit/f4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 	markit/f4.sh  0.1.3.dev.20.39  2020-04-20T21:42:49.008923-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.19  
+# 	   markit/f4.sh -->   begin testing - automate adding file name(s) to beginning of message  
+#
+###
+###
