34da5431341460af21532e5a0ff2570b250657a8
Bradley Allen <allen.bradley@ymail.com>
2020-04-20 19:53:40 -0500

    markit/f1.sh -->   2   test using markit and adding ${FILE_LIST} -->

--- markit/f1.sh
+++ markit/f1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# 	markit/f1.sh  0.1.3.dev.13.32  2020-04-20T19:53:40.036585-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.12  
+# 	   2   test using markit and adding ${FILE_LIST} --> 
 
+#
 #
 #
 #
