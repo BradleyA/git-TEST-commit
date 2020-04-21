098d3a8893faa4f55b4ceec2d35422421fc7462a
Bradley Allen <allen.bradley@ymail.com>
2020-04-20 20:57:24 -0500

    markit/f1.sh markit/f2.sh markit/f3.sh -->   5 test using markit and adding ${FILE_LIST} -->

--- markit/f3.sh
+++ markit/f3.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+# 	markit/f3.sh  0.1.3.dev.17.36  2020-04-20T20:57:24.789719-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.16  
+# 	   markit/f1.sh markit/f2.sh markit/f3.sh -->   5 test using markit and adding ${FILE_LIST} -->  
 # 	markit/f3.sh  0.1.3.dev.16.35  2020-04-20T20:49:46.062842-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.15  
-# 	   markit/f1.sh
-markit/f2.sh
-markit/f3.sh -->   4 test using markit and adding ${FILE_LIST} -->  
 # 	markit/f3.sh  0.1.3.dev.15.34  2020-04-20T20:29:38.328641-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.14  
 # 	markit/f3.sh  0.1.3.dev.12.31  2020-04-20T19:19:50.926120-05:00 (CDT)  https://github.com/BradleyA/git-TEST-commit.git  master  uadmin  five-rpi3b.cptx86.com 0.1.3.dev.11  
 # 	   This is a test using markit and adding ${FILE_LIST} --> 
